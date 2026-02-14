68483e91feecd6e7c8fd4d0833a54799c01524bb
varre1v <163221493+varre1v@users.noreply.github.com>
2026-02-14 07:18:41 -0600

    Create DDL script for Bronze schema tables
    This script creates the bronze schema tables and drops existing ones if they exist. It includes tables for customer information, sales details, product information, and various ERP-related data.

---
+++ scripts/Bronze layer/ddl_bronze.sql
@@ -0,0 +1,90 @@
+/*
+===========================================================================
+DDL Script: Create Bronze tables
+===========================================================================
+Script Purpose: 
+  This script creates tables ini the bronze schema dropping existing tables
+  if they already exist
+  Run this script to re-drfine the DDL structure of bronze table
+===========================================================================
+*/
+
+if object_id('bronze.crm_cust_info', 'U') is not null
+  Drop table bronze.crm_cust_info;
+GO
+
+create table bronze.crm_cust_info(
+cst_id int,
+cst_key nvarchar(50),
+cst_firstname nvarchar(50),
+cst_lastname nvarchar(50),
+cst_marital_status nvarchar(50),
+cst_gndr nvarchar(50),
+cst_create_date date
+);
+GO
+
+if object_id('bronze.crm_sales_details', 'U') is not null
+  Drop table bronze.crm_sales_details;
+GO
+  
+create table bronze.crm_sales_details(
+  sls_ord_num NVARCHAR(50),
+  sls_prd_key NVARCHAR(50),
+  sls_cisut_id INT,
+  sls_order_dt INT,
+  sls_ship_dt INT,
+  sls_due_dt INT,
+  sls_sales INT,
+  sls_quantity INT,
+  sls_price INT
+);
+GO
+  
+if object_id('bronze.crm_prd_info', 'U') is not null
+  Drop table bronze.crm_prd_info;
+GO
+  
+create table bronze.crm_prd_info(
+  prd_id INT,
+  prd_key NVARCHAR(50),
+  prd_nm NVARCHAR(50),
+  prd_cost INT,
+  prd_line NVARCHAR(50),
+  prd_start_dt DATETIME,
+  prd_end_dt DATETIME
+);
+GO 
+  
+if object_id('bronze.erp_loc_a101', 'U') is not null
+  Drop table bronze.erp_loc_a101;
+GO
+
+create table bronze.erp_loc_a101(
+cid NVARCHAR(50),
+cntry NVARCHAR(50)
+);
+GO
+
+if object_id('bronze.erp_cust_az12', 'U') is not null
+  Drop table bronze.erp_cust_az12;
+GO
+
+create table bronze.erp_cust_az12(
+cid NVARCHAR(50),
+bdate DATE,
+gen NVARCHAR(50)
+);
+GO
+
+if object_id('bronze.erp_px_cat_g1v2', 'U') is not null
+  Drop table bronze.erp_px_cat_g1v2;
+GO
+  
+create table bronze.erp_px_cat_g1v2(
+id NVARCHAR(50),
+cat NVARCHAR(50),
+subcat NVARCHAR(50),
+maintenance NVARCHAR(50)
+);
+GO 
