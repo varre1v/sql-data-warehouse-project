6ceb7bf1b30c703ba59184ac597d3c24f0b47a38
varre1v <163221493+varre1v@users.noreply.github.com>
2026-02-17 09:42:55 -0600

    Insert transformed product data into silver layer
    Insert product information into silver.crm_prd_info from bronze.crm_prd_info with transformations.

--- scripts/silver layer/Transform.sql
+++ scripts/silver layer/Transform.sql
@@ -19,3 +19,29 @@ select *,
 row_number() over (partition by cst_id order by cst_create_date desc) as flag_last
 from bronze.crm_cust_info) t
 where flag_last=1
+
+print '================================================='
+insert into silver.crm_prd_info(
+prd_id,
+cat_id,
+prd_key,
+prd_nm,
+prd_cost,
+prd_line,
+prd_start_dt,
+prd_end_dt
+)
+
+select 
+prd_id,
+replace(substring(prd_key, 1,5), '-', '_') as cat_id,
+substring(prd_key, 7, len(prd_key)) as prd_key,
+prd_nm,
+isnull(prd_cost, 0) as prd_cost,
+prd_line,
+cast(prd_start_dt as date) as prd_start_dt,
+cast(lead(prd_start_dt) over ( partition by prd_key order by prd_start_dt)-1 as DATE) as prd_end_dt
+from bronze.crm_prd_info
+--where replace(substring(prd_key, 1,5), '-', '_') not in 
+--(select distinct id from bronze.erp_px_cat_g1v2)
+---select * from bronze.crm_sales_details
